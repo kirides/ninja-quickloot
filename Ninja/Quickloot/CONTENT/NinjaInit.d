d09353a1f5bee1cb44da1c73f02ca77d38ded096
Kirides <john.heckendorf@outlook.com>
2020-08-22 20:42:31 +0200

    Source code cleanup, add support for Gothic 1 and custom looting key

--- Ninja/Quickloot/CONTENT/NinjaInit.d
+++ Ninja/Quickloot/CONTENT/NinjaInit.d
@@ -1,3 +1,14 @@
+/// Menu-Init-function called by Ninja (as soon as the main menu opens)
+func void Ninja_Quickloot_Menu(var int menuPtr) {
+	// Initialize Ikarus
+	const int once = 1;
+	if (once) {
+		MEM_InitAll();
+		Ninja_Quickloot_Init_Options();
+		once = 0;
+	};
+};
+
 /// Init-function called by Ninja
 func void Ninja_Quickloot_Init() {
 	// Initialize Ikarus
